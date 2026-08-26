7e19201a0de2640805ee1b696058ebf91fdbebef
blindpro <noahbalboa34@gmail.com>
2026-08-25 22:47:22 -0500

    Feat: Add parser class.

---
+++ Parser.cs
@@ -0,0 +1,22 @@
+using System.IO;
+public static class Parser
+{
+public static bool ParseFile()
+{
+if(!File.Exists("api.txt"))
+{
+return false;
+}
+foreach(string line in File.ReadLines("api.txt"))
+{
+string[] parts = line.Split(":",StringSplitOptions.RemoveEmptyEntries);
+if(parts.Length<4)
+{
+continue;
+}
+var data = new ApiData(parts[0],parts[1],parts[2],parts[3]);
+Database.add(data);
+}
+return true;
+}
+}
