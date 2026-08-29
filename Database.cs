6e383a398f0302ff8d8eca72596853842ddfc552
blindpro <noahbalboa34@gmail.com>
2026-08-28 19:32:12 -0500

    Feat: Add methods to the database class.

--- Database.cs
+++ Database.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 public static class Database
 {
 public static List<ApiData> Data {get; private set;} = new();
+public static Dictionary<string,Func<string>> Methods {get; private set;} = new();
 public static int 	GetSize()
 {
 return Data.Count;
@@ -9,5 +10,13 @@ return Data.Count;
 public static void add(ApiData data)
 {
 Data.Add(data);
+}
+public static void AddMethod(string name, Func<string> method)
+    {
+Methods[name] = method;
+    }
+public static bool HasMethod(string name)
+    {
+return Methods.ContainsKey(name);
 }
 }
